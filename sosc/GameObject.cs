e7138eb68ccbbc56ddaafd58d0e3e86005ef1fd5
Frederik <fehjorth@gmail.com>
2017-04-06 09:55:38 +0200

    changes

--- sosc/GameObject.cs
+++ sosc/GameObject.cs
@@ -10,8 +10,7 @@ namespace SOSC
     abstract class GameObject
     {
         string tester;
-        string tester2;
-        string tester3;
+
         int one;
         protected Vector2D position;
         Image sprite;
